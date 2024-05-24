475275f1a092ac9bc0cd6e43f135b5a11fd9eee1
Esther <estheryoo118@gmail.com>
2024-05-24 10:30:31 -0400

    update (opcodes)

--- main.c
+++ main.c
@@ -6,11 +6,22 @@
 #include <stdbool.h>
 
 #define MEMORY_SIZE 65535
+#define HIGH_BYTE(reg) ((uint8_t)((reg >> 8) & 0xFF))
+#define LOW_BYTE(reg) ((uint8_t)(reg & 0xFF))
+#define SET_HIGH_BYTE(reg, value) ((reg) = ((reg) & 0x00FF) | ((value) << 8))
 
-unsigned int AF = 0;
-unsigned int BC = 0;
-unsigned int DE = 0;
-unsigned int HL = 0;
+#define CARRY_MASK ((1 << 1) - 1) << 0
+#define PARITY_MASK ((1 << 1)- 1) << 2
+#define AC_MASK ((1 << 1)- 1) << 4
+#define ZERO_MASK ((1 << 1)- 1) << 6
+#define SIGN_MASK ((1 << 1)- 1) << 7
+
+uint16_t AF = 0;
+uint16_t BC = 0;
+uint16_t DE = 0;
+uint16_t HL = 0;
+uint16_t SP = 0;
+uint16_t stack = {0};
 
 size_t fileSize;
 
@@ -47,12 +58,32 @@ unsigned char* loadROM () {
 
 void opcodeExtract (unsigned char* codebuffer, int PC) {
     unsigned char* opcode = &codebuffer[PC];
-    printf("%04x", codebuffer[PC]);
+    printf("%02x", codebuffer[PC]);
+    int opbytes = 1;
+    uint16_t temp16 = 0;
+    uint8_t temp8 = 0;
+    switch (*opcode) {
+        case (0x00): // NOP
+            opbytes = 1;
+            break;
+        case (0x01): // LXI B, d16
+            opbytes = 3;
+            BC = ((uint16_t) codebuffer[PC + 1] << 8) | codebuffer[PC + 2];
+        case (0x02): // STAX B
+            SET_HIGH_BYTE(AF, codebuffer[PC + 1]);
+            break;
+        case (0x03): // INX B
+            BC += 1;
+            break;
+        case (0x04): // INR B (S, Z, A, P)
+            SET_HIGH_BYTE(temp16, (BC & 0xFF00) + 1 | ((BC & 0x00FF)));
+
+    }
 }
 
 int main (int argc, char** argv) {
     unsigned char* buffer = loadROM();
-    int PC = 0;
+    int PC = 3;
     opcodeExtract(buffer, PC);
     return 0;
 }
